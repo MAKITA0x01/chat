c7a173cfa40b5425d4213ec0219e54b3ff9a90fd
Makita0x01 <mailserver0x01@gmail.com>
2019-12-22 02:09:17 +0900

    change main.py

--- main.py
+++ main.py
@@ -9,6 +9,4 @@ def index():
     return render_template('test.html')
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug = True)
