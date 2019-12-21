f6aa0bf472978c30b3c07dc869699d4545519608
Makita0x01 <mailserver0x01@gmail.com>
2019-12-22 02:52:31 +0900

    roleback sakki

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template('test.html')
+    return render_template('test.html',name="hoge")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+#    app.run(debug = True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
